2c39666c33b51a3e2179f05eaa0604af68725cf6
cepps <cepps@regionalmanagement.com>
2021-03-09 16:17:10 -0500

    Create famous_quote_2.py

---
+++ famous_quote_2.py
@@ -0,0 +1,3 @@
+famous_person = "H.P. Lovecraft"
+message = famous_person.title() + ' once said, "The most merciful thing in the world, I think, is the inability of the human mind to correlate all its contents"'
+print(message)
