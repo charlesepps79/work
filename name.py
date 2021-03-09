5e5005f81a4432ee657734f89ada0a5673423cf5
cepps <cepps@regionalmanagement.com>
2021-03-09 16:19:24 -0500

    Create name.py

---
+++ name.py
@@ -0,0 +1,17 @@
+name = "charles epps"
+print(name.title())
+
+name = "Charles Epps"
+print(name.upper())
+print(name.lower())
+
+first_name = "charles"
+last_name = "epps"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
