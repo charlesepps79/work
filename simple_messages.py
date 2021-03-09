2b4b86db97fbb2c3a6e9a6b771ee3422ec513c41
cepps <cepps@regionalmanagement.com>
2021-03-09 16:19:59 -0500

    Create simple_messages.py

---
+++ simple_messages.py
@@ -0,0 +1,5 @@
+lovecraft = "The most merciful thing in the world, I think, is the inability of the human mind to correlate all its contents."
+print(lovecraft)
+
+lovecraft = "The oldest and strongest emotion of mankind is fear, and the oldest and strongest kind of fear is fear of the unknown."
+print(lovecraft)
